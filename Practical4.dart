7b943b4fddda3eb75926191d76bd3a23d20dd7de
japshah <115735951+japshah@users.noreply.github.com>
2024-11-08 16:01:34 +0530

    Practical4.dart

--- Practical4.dart
+++ Practical4.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
           child: Column(
             children: [
               Image.network('https://nuv.ac.in/wp-content/uploads/new-logo.png', width: 400,height: 200),
-              const Image(image: AssetImage('assets/new-logo.png')),
+              const Image(image: AssetImage('assets/SOU.jpg')),
               Text(
                 'Welcome to NUV',
                 style: TextStyle(fontSize: 50.0),
